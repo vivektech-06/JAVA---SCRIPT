e23984d22cc7526ba84f65c40bdf8dcf94638b2e
Vivek Kumar Kandu <vivekk11951@gmail.com>
2026-06-10 08:55:46 +0000

    while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// while (condition) {
+
+// }
+
+let index = 0
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}`)
+//   index = index + 2
+// }
+
+let myArray = ['flash ', 'batman ', 'superman']
+let arr = 0
+while (arr < myArray.length) {
+ // console.log(`Value is ${myArray[arr]}`)
+  arr = arr + 1
+}
+
+// let Score = 1 
+// do {
+//     console.log(`Score is ${Score}`);
+//     Score++ 
+// } while (Score <=10);
+
+let Score = 11 
+do {                                        // in do while loop atleat one time always executed 
+    console.log(`Score is ${Score}`);
+    Score++ 
+} while (Score <=10);
