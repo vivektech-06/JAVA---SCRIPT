98a4d8673a4bd313c7f5f283b082b83a5c93b3a9
Vivek Kumar Kandu <vivekk11951@gmail.com>
2026-06-10 10:40:28 +0000

    in

---
+++ 05_iterations/six.js
@@ -0,0 +1,10 @@
+// const coding = ['JS', 'JAVA', 'RB', 'CPP', 'PYTHON']
+
+
+// const values = coding.forEach((item)=>{
+//     console.log(item)
+// })
+// console.log(values)
+
+const myNum = [ 1,2,3,4,5,6,7,8,9,10]
+myNum
